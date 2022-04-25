5f88eb0db0cf493149fa9cbe072beedf855202d4
Thamognya Kodi <contact@thamognya.com>
2022-04-25 08:59:58 +0700

    update

--- mirror.sh
+++ mirror.sh
@@ -11,13 +11,21 @@ echo "DESTINATION=$DESTINATION_REPO"
 git config --global user.email "$EMAIL"
 git config --global user.name "$NAME"
 git clone "$DESTINATION_REPO" "$DESTINATION_DIR"
+echo "cloned"
 cd "$DESTINATION_DIR"
+echo "entered directory"
 rm -rf *
+echo "removed all files"
 git clone "$SOURCE_REPO" "$SOURCE_DIR"
+echo "cloned"
 mv "$SOURCE_DIR"/* .
+echo "moved files"
 rm -rf "$SOURCE_DIR"
+echo "removed source directory"
 git add .
-git commit -m 'auto update'
+echo "added files"
+git commit -m "auto update"
+echo "committed"
 git push origin "$BRANCH" --force
-
+echo "pushed"
 echo "done"
