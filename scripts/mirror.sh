2551944844997151e9a90f743c3433aadecc4a1e
Thamognya Kodi <contact@thamognya.com>
2022-04-25 08:31:58 +0700

    update

--- scripts/mirror.sh
+++ scripts/mirror.sh
@@ -14,9 +14,21 @@ git config --global user.name "$NAME"
 
 git clone "$SOURCE_REPO" "$SOURCE_DIR" 
 cd "$SOURCE_DIR"
-git remote add destination "$DESTINATION_REPO"
-git add .
-git commit -m 'auto update'
-git push destination master --force
+#git remote add destination "$DESTINATION_REPO"
+#git add .
+#git commit -m 'auto update'
+#git push destination master --force
+git clone --mirror "$SOURCE_REPO" "$SOURCE_DIR" && cd "$SOURCE_DIR"
+git remote set-url --push origin "$DESTINATION_REPO"
+git fetch -p origin
+# Exclude refs created by GitHub for pull request.
+git for-each-ref --format 'delete %(refname)' refs/pull | git update-ref --stdin
+if [ "$DRY_RUN" = "true" ]
+then
+    echo "INFO: Dry Run, no data is pushed"
+    git push --mirror --dry-run
+else
+    git push --mirror
+fi
 
 echo "done"
